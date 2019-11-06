5b9c5a894fe295c41c8d6e683d96660cfd47dddb
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-06 13:38:06 +0100

    Add files via upload

---
+++ html/bde.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>BDE CESI</title>
+		<link rel="stylesheet" href="css/style.css">
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<header>
+ 			<img title="Retour à la page d'accueil" alt="Retour à la page d'accueil" src="images/logo.png">
+		</header>
+		<h1>Créer un évènement:</h1>
+	<form method="get" action="events.php" >
+        <p>
+        <textarea name="message" rows="8" cols="45">
+                
+        </textarea>
+            <input type="submit" value="Valider" /></p>
+</form>
+
+	</body>
+</html>
+
+
---
+++ html/login.php
@@ -0,0 +1,16 @@
+<!doctype html>
+<html lang="fr">
+	<head>
+		<meta charset="utf-8">
+		<title>BDE CESI</title>
+		<link rel="stylesheet" href="css/style.css">
+		<script src="script.js"></script>
+	</head>
+	<body>
+		<header>
+ 			<img title="Retour à la page d'accueil" alt="Retour à la page d'accueil" src="images/logo.png">
+		</header>
+		<h1>Connexion</h1>
+
+	</body>
+</html>

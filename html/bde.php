7bd46f1020d95e38aeefddf79747e1693cc80747
Pierrot-la-menace <pierre.garrido@viacesi.fr>
2019-11-12 14:03:15 +0100

    Adding administration features

--- html/bde.php
+++ html/bde.php
@@ -1,26 +1,13 @@
 <?php 
+require 'php/header.php';
 if(!isset($_SESSION['id']))
 {
   header('Location: index.php');
 }
-else if($_SESSION['droit'] == 2)
+else if($_SESSION['droit'] != 2)
 {
   header('Location: index.php');
 } ?>
-<!doctype html>
-<html lang="fr">
-	<head>
-		<meta charset="utf-8">
-		<title>BDE CESI</title>
-		<link rel="stylesheet" href="css/bootstrap.min.css">
-		<link rel="stylesheet" href="css/style.css">
-		<link rel="icon" type="image/png" href="images/favicon.png" />
-		<script src="script.js"></script>
-	</head>
-	<body>
-		<header>
- 			<img title="Retour à la page d'accueil" alt="Retour à la page d'accueil" src="images/logo.png">
-		</header>
 		<h1>Page d'administration</h1>
 		<?php 
 		if(!isset($_GET['compte']))
@@ -29,19 +16,28 @@ else if($_SESSION['droit'] == 2)
 		  
 		  if($req->columnCount() == 0)
 		  {
-		    echo '<p id="result_compte">Aucune compte actif. Erreur BDD';
+		    echo '<p id="result_compte">Aucune compte actif. Erreur BDD</p>';
 		  }
 		  else
 		  {
  		    echo '<p id="result_compte">';
 		    while($donnees = $req->fetch())
 		    {
-			echo '<span class="entite_compte">';
-                        echo '';
+			echo '<span class="entite_compte">Compte : ' . $donnees['MAIL']. ' ID : ' . $donnees['ID_MEMBRE']. ' <a href="bde.php?compte="' . $donnees['MAIL'] . '">Gérer</a></span><br />';
 		    }
+                    echo '</p>';
+                  }
+		  $req->closeCursor();
 		  
-		} ?>
+		}
+		else
+		{ 
+		  $compte = htmlspecialchars((string)$_GET['compte']);
+		  $req = $bdd->prepare('SELECT * FROM MEMBRE WHERE MAIL = ?');
+		  $req->execute(array($compte));
+		  $donnee = $req->fetch();
+                ?>
+		  <form method="post">
+                  </form>
 	</body>
 </html>
-
-

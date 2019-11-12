de7f4eab9591f5d4ebeb19737b59961ba3ec7f40
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-12 13:39:06 +0100

    Update events.php

--- html/events.php
+++ html/events.php
@@ -4,7 +4,7 @@
 		<p>Cette page regroupe les différents évènements qui vous sont proposés par le bureau des étudiants :</p>
 		<article>
 			<h2> Titre </h2>
-			<img class="center" src=" path img">
+			<img class="center" src="https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&cad=rja&uact=8&ved=2ahUKEwjt0sK62OTlAhWk8uAKHe3PAgQQjRx6BAgBEAQ&url=%2Furl%3Fsa%3Di%26rct%3Dj%26q%3D%26esrc%3Ds%26source%3Dimages%26cd%3D%26ved%3D%26url%3Dhttp%253A%252F%252Fwww.radioprevert.com%252Fleo-et-esteban-vous-presentent-le-laser-game%252F%26psig%3DAOvVaw1FgrSGvsN-4vXl2MEPbYVc%26ust%3D1573648713255911&psig=AOvVaw1FgrSGvsN-4vXl2MEPbYVc&ust=1573648713255911">
 			<br/>
 			<p> 	Vous pouvez personnaliser la description de l'événement pour promouvoir votre événement
 				et fournir aux participants potentiels les informations les plus pertinentes susceptibles
@@ -14,7 +14,20 @@
 				à la section « Description de l'événement » sous Étape 1 : description de l'événement pour
 				commencer.
 			</p>
-			
+		</article>
+		<article>
+			<h2> Titre 2</h2>
+			<img class="center" src="https://images.sudouest.fr/2019/07/24/5d382eb966a4bd41609b31ce/widescreen/1000x500/deux-versions-de-la-soiree-s-opposent.jpg?v1">
+			<br/>
+			<p> 	Vous pouvez personnaliser la description de l'événement pour promouvoir votre événement
+				et fournir aux participants potentiels les informations les plus pertinentes susceptibles
+				de les intéresser. Assurez-vous d'inclure une description détaillée, et intégrez des FAQ
+				avec des informations sur les options de transport, les informations de contact ainsi qu'
+				une politique de remboursement. À partir de la page Modifier de votre événement, rendez-vous
+				à la section « Description de l'événement » sous Étape 1 : description de l'événement pour
+				commencer.
+			</p>
+		</article>
 	</body>
 </html>
 

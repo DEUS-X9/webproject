59bfbeff0fe5ceedc8a49b47dbccbd554a95cd70
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-12 11:25:08 +0100

    Update events.php

--- html/events.php
+++ html/events.php
@@ -2,7 +2,18 @@
 
 		<h1>Evènements:</h1>
 		<p>Cette page regroupe les différents évènements qui vous sont proposés par le bureau des étudiants</p>
-
+		<article>
+			<h2> Titre </h2>
+			<img class="" src=" path img">
+			<p> 	Vous pouvez personnaliser la description de l'événement pour promouvoir votre événement
+				et fournir aux participants potentiels les informations les plus pertinentes susceptibles
+				de les intéresser. Assurez-vous d'inclure une description détaillée, et intégrez des FAQ
+				avec des informations sur les options de transport, les informations de contact ainsi qu'
+				une politique de remboursement. À partir de la page Modifier de votre événement, rendez-vous
+				à la section « Description de l'événement » sous Étape 1 : description de l'événement pour
+				commencer.
+			</p>
+			
 	</body>
 </html>
 

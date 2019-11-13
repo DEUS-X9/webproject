b6b65e36f338c8515a0d4f4d3a0a76faee444141
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-13 10:10:30 +0100

    Update header.php

--- html/php/header.php
+++ html/php/header.php
@@ -58,13 +58,13 @@ if(isset($_COOKIE['mail']) AND isset($_COOKIE['mdp']) AND !isset($_SESSION['id']
         			<img title="Retour à la page d'accueil"  src="images/logo.png">
      			</a>
 
-    <?php
+    <div classe="info"> <?php
     if(isset($_SESSION['nom']))
     {
 	echo 'Bienvenue ' . $_SESSION['prenom'] . ' ' . $_SESSION['nom'];
     }
     $pageEnCours = $_SERVER['PHP_SELF']; ?>
-
+    </div>
     <nav class="nav nav-pills nav-justified flex-column flex-sm-row">
 	<a class="nav-link nav-item <?php if($pageEnCours == "/webprojet/index.php") { echo "active";} ?>" href="index.php">Accueil</a>
 	<a class="nav-link nav-item <?php if($pageEnCours == "/webprojet/events.php") { echo "active";} ?>" href="events.php">Evenements</a>

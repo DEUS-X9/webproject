bf82f4f8ef5ff2c16a2ec40c83564f111ae2a93d
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-13 09:55:01 +0100

    Update header.php

--- html/php/header.php
+++ html/php/header.php
@@ -43,8 +43,8 @@ if(isset($_COOKIE['mail']) AND isset($_COOKIE['mdp']) AND !isset($_SESSION['id']
   <meta charset="utf-8">
   <title>BDE CESI</title>
 	
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/css/bootstrap.min.css">
-  <link rel="stylesheet" href="css/style.css">
+   <link rel="stylesheet" href="css/bootstrap.min.css">
+   <link rel="stylesheet" href="css/style.css">
 	
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
   <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.0/js/bootstrap.min.js"></script>	

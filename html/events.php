012c4563cbc8a2b13ac8f39cad80282a13185a01
Pierrot-la-menace <pierre.garrido@viacesi.fr>
2019-11-14 09:05:27 +0100

    Adding events

--- html/events.php
+++ html/events.php
@@ -1,34 +1,300 @@
-<?php require 'php/header.php'; ?>
+<?php require 'php/header.php'; 
+function month($month_p) {
 
-		<h1>Evènements:</h1>
-		<p>Cette page regroupe les différents évènements qui vous sont proposés par le bureau des étudiants :</p>
-		<article>
-			<h2> Titre </h2>
-			<img class="center" src="https://www.passeo.fr/plugins/timthumb.php?src=uploads/20/i4bfTZhcDw-212-BD-LGValence-2011.11.png&h=400&w=789&zc=0">
-			<br/>
-			<p> 	Vous pouvez personnaliser la description de l'événement pour promouvoir votre événement
-				et fournir aux participants potentiels les informations les plus pertinentes susceptibles
-				de les intéresser. Assurez-vous d'inclure une description détaillée, et intégrez des FAQ
-				avec des informations sur les options de transport, les informations de contact ainsi qu'
-				une politique de remboursement. À partir de la page Modifier de votre événement, rendez-vous
-				à la section « Description de l'événement » sous Étape 1 : description de l'événement pour
-				commencer.
-			</p>
-		</article>
-		<article>
-			<h2> Titre 2</h2>
-			<img class="center" src="https://images.sudouest.fr/2019/07/24/5d382eb966a4bd41609b31ce/widescreen/1000x500/deux-versions-de-la-soiree-s-opposent.jpg?v1">
-			<br/>
-			<p> 	Vous pouvez personnaliser la description de l'événement pour promouvoir votre événement
-				et fournir aux participants potentiels les informations les plus pertinentes susceptibles
-				de les intéresser. Assurez-vous d'inclure une description détaillée, et intégrez des FAQ
-				avec des informations sur les options de transport, les informations de contact ainsi qu'
-				une politique de remboursement. À partir de la page Modifier de votre événement, rendez-vous
-				à la section « Description de l'événement » sous Étape 1 : description de l'événement pour
-				commencer.
-			</p>
-		</article>
+  switch($month_p) {
+     case 1:
+        return 'janvier';
+        break;
+     case 2:
+        return 'février';
+        break;
+     case 3:
+        return 'mars';
+        break;
+     case 4:
+        return 'avril';
+        break;
+     case 5:
+        return 'mai';
+        break;
+     case 6:
+        return 'juin';
+        break;
+     case 7:
+        return 'juillet';
+        break;
+     case 8:
+        return 'août';
+        break;
+     case 9:
+        return 'septembre';
+        break;
+     case 10:
+        return 'octobre';
+        break;
+     case 11:
+        return 'novembre';
+        break;
+     case 12:
+        return 'décembre';
+        break;
+  }
+}
+                if(!isset($_GET['id_event']) AND !isset($_GET['ajout']))
+                { 
+                    if(!isset($_GET['page']))
+                    { 
+                    ?>
+		      <h1>Evènements</h1>
+		      <p>Ces pages regroupent les différents évènements qui vous sont proposés et qui ont été proposés par le bureau des étudiants :</p>
+                      <?php
+                      if(!isset($_SESSION['id_region']))
+                      {
+                      	$req = $bdd->query('SELECT ID_EVENTS, EVENTS, E_DESCRIPTION, DAY(E_DATE) AS jour, MONTH(E_DATE) AS mois, YEAR(E_DATE) AS annee, CHEMIN, REGION FROM EVENEMENTS INNER JOIN PHOTO ON EVENEMENTS.ID_PHOTO = PHOTO.ID_PHOTO INNER JOIN REGION ON REGION.ID_REGION = EVENEMENTS.ID_REGION ORDER BY E_DATE DESC LIMIT 0, 5');
+                      }
+                      else
+                      {
+                        $req = $bdd->query('SELECT ID_EVENTS, EVENTS, E_DESCRIPTION, DAY(E_DATE) AS jour, MONTH(E_DATE) AS mois, YEAR(E_DATE) AS annee, CHEMIN FROM EVENEMENTS INNER JOIN PHOTO ON EVENEMENTS.ID_PHOTO = PHOTO.ID_PHOTO WHERE ID_REGION = ' . $_SESSION['id_region'] . ' ORDER BY E_DATE DESC LIMIT 0, 5');
+                      }
+                      $events = $req->fetchAll();
+                      foreach($events as $event)
+                      {
+                      ?>
+		        <article>
+		           <a href="events.php?id_event=<?php echo $event['ID_EVENTS']; ?>"><h2> <?php echo $event['EVENTS']; ?> </h2></a>
+			   <img class="center" src="photos/<?php echo $event['CHEMIN']; ?>">
+		 	   <br/>
+                           <span class="metadata">Le <?php echo $event['jour'] ?> <?php echo month($event['mois']); ?> <?php echo $event['annee']; ?></span><br />
+                           <?php
+                           if(!isset($_SESSION['id_region']))
+                           { ?>
+                           <span class="metadata">A <?php echo $event['REGION'] ?></span><br /><br />
+                           <?php
+                           }
+                           ?>
+			   <p><?php echo nl2br(htmlspecialchars($event['E_DESCRIPTION'])); ?>
+			   </p>
+		       </article>
+                       <?php
+                      }
+                      $req->closeCursor();
+                      
+                      $req = $bdd->query('SELECT * FROM EVENEMENTS');
+                      $donnee = $req->fetchAll();
+                      $i = 0;
+                      foreach($donnee as $event)
+                      {
+                        $i++;
+                      }
+                      $req->closeCursor();
+
+                      $mod = $i%5;
+                      $nbpage = ($i - $mod)/5;
+                      $nbpage++;
+                      
+                      $j = 1;
+
+                      while($j <= $nbpage)
+                      {
+                        if($j == 1)
+                        {
+                           echo '<p>Page : <a href="#">1</a> ';
+                        }
+                        else
+                        {
+                           echo '- <a href="events.php?page=' . $j . '">' . $j . '</a> ';
+                        }
+                        $j++;
+                       }
+                       echo '</p>';
+                     }
+                     else
+                     {
+                       $page = (int)$_GET['page'];
+                       if($page <= 1)
+                       {
+                         header('Location: events.php');
+                       }
+                       else
+                       { ?>
+                         <h1>Evènements</h1>
+		         <p>Ces pages regroupent les différents évènements qui vous sont proposés et qui ont été proposés par le bureau des étudiants :</p>
+                       <?php
+                         $debut = ($page - 1) * 5;
+                         $fin = $page * 5;
+
+                         if(!isset($_SESSION['id_region']))
+                         {
+                      	   $req = $bdd->query('SELECT ID_EVENTS, EVENTS, E_DESCRIPTION, DAY(E_DATE) AS jour, MONTH(E_DATE) AS mois, YEAR(E_DATE) AS annee, CHEMIN, REGION FROM EVENEMENTS INNER JOIN PHOTO ON EVENEMENTS.ID_PHOTO = PHOTO.ID_PHOTO INNER JOIN REGION ON REGION.ID_REGION = EVENEMENTS.ID_REGION ORDER BY E_DATE DESC LIMIT ' . $debut . ', ' . $fin . '');
+                         }
+                         else
+                         {
+                           $req = $bdd->query('SELECT ID_EVENTS, EVENTS, E_DESCRIPTION, DAY(E_DATE) AS jour, MONTH(E_DATE) AS mois, YEAR(E_DATE) AS annee, CHEMIN FROM EVENEMENTS INNER JOIN PHOTO ON EVENEMENTS.ID_PHOTO = PHOTO.ID_PHOTO WHERE ID_REGION = ' . $_SESSION['id_region'] . ' ORDER BY E_DATE DESC LIMIT ' . $debut . ', ' . $fin . '');
+                         }
+                     
+                         $events = $req->fetchAll();
+    			 foreach($events as $event)
+                         {
+                         ?>
+		           <article>
+		              <a href="events.php?id_event=<?php echo $event['ID_EVENTS']; ?>"><h2> <?php echo $event['EVENTS']; ?> </h2></a>
+			      <img class="center" src="photos/<?php echo $event['CHEMIN']; ?>">
+		 	      <br/>
+                              <span class="metadata">Le <?php echo $event['jour'] ?> <?php echo month($event['mois']); ?> <?php echo $event['annee']; ?></span><br />
+                              <?php
+                              if(!isset($_SESSION['id_region']))
+                              { ?>
+                              <span class="metadata">A <?php echo $event['REGION'] ?></span><br /><br />
+                              <?php
+                              }
+                              ?>
+			      <p><?php echo nl2br(htmlspecialchars($event['E_DESCRIPTION'])); ?></p>
+		           </article>
+                         <?php
+                         }
+                         $req->closeCursor();
+                      
+                         $req = $bdd->query('SELECT * FROM EVENEMENTS');
+                         $donnee = $req->fetchAll();
+                         $i = 0;
+                         foreach($donnee as $event)
+                         {
+                           $i++;
+                         }
+                         $req->closeCursor();
+
+                         $mod = $i%5;
+                         $nbpage = ($i - $mod)/5;
+                         $nbpage++;
+                      
+                         $j = 1;
+
+                        while($j <= $nbpage)
+                        {
+                          if($j == 1)
+                          {
+                             echo '<p>Page : <a href="events.php">1</a> ';
+                          }
+                          else
+                          {
+                             echo '- <a href="events.php?page=' . $j . '">' . $j . '</a> ';
+                          }
+                          $j++;
+                         }
+                         echo '</p>';                  
+                       } 
+                    }                       
+                 }
+                 else if(isset($_GET['ajout']))
+                 {
+                   
+                 }
+                 else if(isset($_GET['id_event']))
+                 {
+                   $e_id = (int)$_GET['id_event'];
+                   if($e_id < 1)
+                   {
+                     header('Location: events.php');
+                   }
+                   else
+                   {
+                      $req = $bdd->prepare('SELECT ID_EVENTS, EVENTS, E_DESCRIPTION, EVENEMENTS.ID_PHOTO, DAY(E_DATE) AS jour, MONTH(E_DATE) AS mois, YEAR(E_DATE) AS annee, CHEMIN, EVENEMENTS.ID_REGION, REGION FROM EVENEMENTS INNER JOIN PHOTO ON EVENEMENTS.ID_PHOTO = PHOTO.ID_PHOTO INNER JOIN REGION ON REGION.ID_REGION = EVENEMENTS.ID_REGION WHERE ID_EVENTS = ?');
+                      $req->execute(array($e_id));
+                      if(!$donnee = $req->fetch())
+                      {
+                        header('Location: events.php');
+                        $req->closeCursor();
+                      }
+                      else
+                      {
+                        if(isset($_SESSION['id_region']) AND $_SESSION['id_region'] != $donnee['ID_REGION'])
+                        {
+                          header('Location: events.php');
+                          $req->closeCursor();
+                        }
+                        else
+                        {
+                        ?>
+                          <h1><?php echo $donnee['EVENTS']; ?></h1>
+                          <p>Le <?php echo $donnee['jour'] ?> <?php echo month($donnee['mois']); ?> <?php echo $donnee['annee']; ?></p>
+                          <?php
+                          if(!isset($_SESSION['id_region']))
+                          {?>
+                          <span class="metadata">A <?php echo $donnee['REGION'] ?></span><br /><br />
+                          <?php
+                          }
+                          ?>
+                          <p><?php echo nl2br(htmlspecialchars($donnee['E_DESCRIPTION'])); ?></p>
+                          <img class="center" src="photos/<?php echo $donnee['CHEMIN']; ?>">
+                          <?php
+                          $req2 = $bdd->prepare('SELECT COMMENTAIRE, NOM, PRENOM FROM COMMENTAIRE INNER JOIN MEMBRE ON MEMBRE.ID_MEMBRE = COMMENTAIRE.ID_MEMBRE WHERE ID_PHOTO = ? ORDER BY date DESC LIMIT 0, 1');
+                          $req2->execute(array($donnee['ID_PHOTO']));
+                          if(!$donnee2 = $req2->fetch())
+                          {?>
+                             <span>Pas de commentaire</span>
+                          <?php
+                          }
+                          else
+                          {?>
+                            <p>Commentaires :<br /><?php echo $donnee2['NOM'] . ' ' . $donnee2['PRENOM'] . ' : ' . $donnee2['COMMENTAIRE'];?></p>
+                          <?php
+                          }
+                          $req2->closeCursor(); 
+                          $req2 = $bdd->prepare('SELECT COUNT(*) AS nb FROM LIKES WHERE ID_PHOTO = ?');
+                          $req2->execute(array($donnee['ID_PHOTO']));
+                          $donnee2 = $req2->fetch();
+                          ?>
+                          <p>Like(s) : <?php echo $donnee2['nb']; ?>. <a href="#" id="Bphoto<?php echo $donnee['ID_PHOTO'];?>">Liker</a>
+                          <?php
+                          $req2->closeCursor(); 
+                          $req->closeCursor();
+                          $req = $bdd->prepare('SELECT ILLUSTRER.ID_PHOTO, CHEMIN, NOM, PRENOM FROM ILLUSTRER INNER JOIN PHOTO ON ILLUSTRER.ID_PHOTO = PHOTO.ID_PHOTO INNER JOIN MEMBRE ON MEMBRE.ID_MEMBRE = PHOTO.ID_MEMBRE WHERE ID_EVENTS = ?');
+                          $req->execute(array($e_id));
+                          $donnees = $req->fetchAll();
+                          if($req->columnCount() == 0)
+                          {?>
+                             <p>Pas de photos postées pour cet event.</p>
+                          <?php
+                          }
+                          else
+                          {?>
+                             <p>Photos postées de l'event :</p>
+                             <?php
+                              foreach($donnees as $image)
+                              {?>
+                                 <p>Posté par <?php echo $image['NOM'] . ' ' . $image['PRENOM']?></p>
+                                 <img class="center" src="photos/<?php echo $image['CHEMIN']; ?>">
+                                 <?php
+                                 $req2 = $bdd->prepare('SELECT COMMENTAIRE, NOM, PRENOM FROM COMMENTAIRE INNER JOIN MEMBRE ON MEMBRE.ID_MEMBRE = COMMENTAIRE.ID_MEMBRE WHERE ID_PHOTO = ? ORDER BY date DESC LIMIT 0, 1');
+                                 $req2->execute(array($image['ID_PHOTO']));
+                              
+                                 if(!$donnee2 = $req2->fetch())
+                                 {?>
+                                   <p>Pas de commentaire</p>
+                                 <?php
+                                 }
+                                 else
+                                 {
+                                    echo $donnee2['NOM'] . ' ' . $donnee2['PRENOM'] . ' : ' . $donnee2['COMMENTAIRE'];
+                                 }
+                                 $req2->closeCursor(); 
+                                 $req2 = $bdd->prepare('SELECT COUNT(*) AS nb FROM LIKES WHERE ID_PHOTO = ?');
+                                 $req2->execute(array($donnee['ID_PHOTO']));
+                                 $donnee2 = $req2->fetch();
+                                 ?>
+                                 <p>Like(s) : <?php echo $donnee2['nb']; ?>. <a href="#" id="Bphoto<?php echo $image['ID_PHOTO'];?>">Liker</a>
+                                 <p>Photos postées de l'event :</p>
+                                 <?php
+                                 $req2->closeCursor();  
+                              }
+                          }
+                          $req->closeCursor();?> 
+                          <form>
+                        <?php
+                        }
+                     }
+                   }
+                 }
+             ?>
 	</body>
 </html>
-
-

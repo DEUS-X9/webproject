f9313997679d5db9221e5166e61fbb3e390b784f
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-17 17:01:11 +0100

    Update basket.php

--- html/basket.php
+++ html/basket.php
@@ -1,8 +1,11 @@
 		<?php require('php/header.php'); ?>
 		<h1>Mon panier</h1>
 		<div class="item">
-			<a href="PAGE DE L'ARTICLE"><h4>nOM DE L'ARTICLE</h4></a>
-			<p>Prix : PRIX DE L'ARTICLE            QUANTITÉE</p>
+			<a href="PAGE DE L'ARTICLE"><h4>NOM DE L'ARTICLE</h4></a>
+			<div class="flex-container">
+				<div>Prix : PRIX DE L'ARTICLE/div>
+				<div>QUANTITÉE</div>
+			</div>
 		</div>
 	</body>
 </html>

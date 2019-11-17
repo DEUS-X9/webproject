56625f686f41b35699f8b6d9441bd0e9fa515f34
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-17 18:53:37 +0100

    Update index.php

--- html/index.php
+++ html/index.php
@@ -4,6 +4,6 @@
 <h1>Bureau des étudiants CESI</h1>
 <img   src="images/bde.svg" height="20%" width="20%" padding="20px"  class="center">
 <br/>
-<h3>Un bureau des étudiants ou bureau des élèves, est une association étudiante d'une même université ou école, élue par leurs adhérents, et qui s'occupe d'organiser les activités extra-scolaires .</h3>
+<p>Un bureau des étudiants ou bureau des élèves, est une association étudiante d'une même université ou école, élue par leurs adhérents, et qui s'occupe d'organiser les activités extra-scolaires .</p>
 </body>
 </html>

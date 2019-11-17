8b070c44c93522b571ac0a75f10ef87b0c8d5fb4
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-17 21:38:43 +0100

    Create footer.php

---
+++ html/php/footer.php
@@ -0,0 +1,3 @@
+<div class="footer">En utilisant ce site, vous acceptez d'avoir lu et accepté nos conditions d'utilisation, notre politique en matière de cookies et de confidentialité.
+<a href="legale.txt">Mentions légales</a>
+</div>

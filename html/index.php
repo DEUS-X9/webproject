b1025a8e0410c25bb63140f4fa526f9acaab2061
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-17 21:39:56 +0100

    Update index.php

--- html/index.php
+++ html/index.php
@@ -5,5 +5,6 @@
 <img   src="images/bde.svg" height="20%" width="20%" padding="20px"  class="center">
 <br/>
 <p>Un bureau des étudiants ou bureau des élèves, est une association étudiante d'une même université ou école, élue par leurs adhérents, et qui s'occupe d'organiser les activités extra-scolaires .</p>
+<?php require 'php/footer.php'; ?>
 </body>
 </html>

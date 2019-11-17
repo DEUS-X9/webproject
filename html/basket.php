24a460c5c6f66cc2cff0a22e72f84e63464a83cb
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-17 16:58:05 +0100

    Update basket.php

--- html/basket.php
+++ html/basket.php
@@ -1,5 +1,8 @@
-<?php require('php/header.php'); ?>
+		<?php require('php/header.php'); ?>
 		<h1>Mon panier</h1>
-
+		<div class="item">
+			<a href="PAGE DE L'ARTICLE"><h4>nOM DE L'ARTICLE</h4></a>
+			<p>Prix : PRIX DE L'ARTICLE            QUANTITÉE</p>
+		</div>
 	</body>
 </html>

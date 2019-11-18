857946668cd9b3c7f1c0c4ca12f7c50662747e7f
pierre122 <pierre.garrido@viacesi.fr>
2019-11-18 10:52:51 +0100

    Correcting bugs

---
+++ html/legal.php
@@ -0,0 +1,10 @@
+
+<?php require 'php/header.php'; ?>
+
+<h1>Bureau des étudiants CESI</h1>
+<img   src="images/bde.svg" height="20%" width="20%" padding="20px"  class="center">
+<br/>
+<p>Un bureau des étudiants ou bureau des élèves, est une association étudiante d'une même université ou école, élue par leurs adhérents, et qui s'occupe d'organiser les activités extra-scolaires .</p>
+<?php require 'php/footer.php'; ?>
+</body>
+</html>

ea0adf2950893207753ef478821c79edf1542c99
GotokuNeko <45080391+GotokuNeko@users.noreply.github.com>
2019-11-18 10:58:12 +0100

    Update footer.php

--- html/php/footer.php
+++ html/php/footer.php
@@ -1,3 +1,8 @@
+<br/>
+<br/>
+<br/>
+
+
 <div class="footer">En utilisant ce site, vous acceptez d'avoir lu et accepté nos conditions d'utilisation, notre politique en matière de cookies et de confidentialité.
 <a href="legal.php">Mentions légales</a>
 </div>
